fd87dfbf21de6b3417ee81bc1a1f2134c53a2a01
zonesgame <32406374+zonesgame@users.noreply.github.com>
2021-07-10 16:34:13 +0800

    Update EventFactory.java

--- core/src/games/stendhal/client/events/EventFactory.java
+++ core/src/games/stendhal/client/events/EventFactory.java
@@ -17,6 +17,7 @@ import games.stendhal.client.entity.RPEntity;
 import games.stendhal.common.constants.Events;
 import marauroa.common.Logger;
 import marauroa.common.game.RPEvent;
+import temp.Debug;
 
 /**
  * creates RPEvent handler
@@ -24,107 +25,112 @@ import marauroa.common.game.RPEvent;
  * @author hendrik
  */
 class EventFactory {
-	private static final Logger logger = Logger.getLogger(EventFactory.class);
+    private static final Logger logger = Logger.getLogger(EventFactory.class);
 
-	/**
-	 * creates events for Entities and RPEntities
-	 * @param entity
-	 * @param rpevent
-	 * @return event
-	 */
-	static Event<? extends Entity> create(Entity entity, RPEvent rpevent) {
-		Event<? extends Entity> res = null;
-		if (entity instanceof RPEntity) {
-			res = createEventsForRPEntity((RPEntity)entity, rpevent);
-		}
+    /**
+     * creates events for Entities and RPEntities
+     *
+     * @param entity
+     * @param rpevent
+     * @return event
+     */
+    static Event<? extends Entity> create(Entity entity, RPEvent rpevent) {
+        Event<? extends Entity> res = null;
+        if (entity instanceof RPEntity) {
+            res = createEventsForRPEntity((RPEntity) entity, rpevent);
+        }
 
-		if (res == null) {
-			res = createEventsForEntity(entity, rpevent);
-		}
+        if (res == null) {
+            res = createEventsForEntity(entity, rpevent);
+        }
 
-		if (res == null) {
-			Event<Entity> unknown = new UnknownEvent<Entity>();
-			unknown.init(entity, rpevent);
-			res = unknown;
-		}
-		logger.debug("Created event: "+res);
-		return res;
-	}
+        if (res == null) {
+            Event<Entity> unknown = new UnknownEvent<Entity>();
+            unknown.init(entity, rpevent);
+            res = unknown;
+        }
+        logger.debug("Created event: " + res);
+        return res;
+    }
 
-	/**
-	 * Creates events for normal RPEntities.
-	 *
-	 * @param entity  RPEntityEntity
-	 * @param rpevent RPEvent
-	 * @return Event handler
-	 */
-	private static Event<RPEntity> createEventsForRPEntity(RPEntity entity, RPEvent rpevent) {
-		String name = rpevent.getName();
-		Event<RPEntity> event = null;
-		if (name.equals(Events.PUBLIC_TEXT)) {
-			event = new PublicTextEvent();
-		} else if (name.equals(Events.PRIVATE_TEXT)) {
-			event = new PrivateTextEvent();
-		} else if (name.equals(Events.ATTACK)) {
-			event = new AttackEvent();
-		} else if (name.equals(Events.TRADE_STATE_CHANGE)) {
-			event = new TradeStateChangeEvent();
-		} else if (name.equals(Events.GROUP_CHANGE)) {
-			event = new GroupChangeEvent();
-		} else if (name.equals(Events.GROUP_INVITE)) {
-			event = new GroupInviteEvent();
-		} else if (name.equals(Events.PROGRESS_STATUS_CHANGE)) {
-			event = new ProgressStatusEvent();
-		} else if (name.equals(Events.REACHED_ACHIEVEMENT)) {
-			event = new ReachedAchievementEvent();
-		} else if (name.equals(Events.BESTIARY)) {
-			event = new BestiaryEvent();
-		}
+    /**
+     * Creates events for normal RPEntities.
+     *
+     * @param entity  RPEntityEntity
+     * @param rpevent RPEvent
+     * @return Event handler
+     */
+    private static Event<RPEntity> createEventsForRPEntity(RPEntity entity, RPEvent rpevent) {
+        String name = rpevent.getName();
+        Event<RPEntity> event = null;
+        if (name.equals(Events.PUBLIC_TEXT)) {
+            event = new PublicTextEvent();
+        } else if (name.equals(Events.PRIVATE_TEXT)) {
+            event = new PrivateTextEvent();
+        } else if (name.equals(Events.ATTACK)) {
+            event = new AttackEvent();
+        } else if (name.equals(Events.TRADE_STATE_CHANGE)) {
+            event = new TradeStateChangeEvent();
+        } else if (name.equals(Events.GROUP_CHANGE)) {
+            event = new GroupChangeEvent();
+        } else if (name.equals(Events.GROUP_INVITE)) {
+            event = new GroupInviteEvent();
+        } else if (name.equals(Events.PROGRESS_STATUS_CHANGE)) {
+            event = new ProgressStatusEvent();
+        } else if (name.equals(Events.REACHED_ACHIEVEMENT)) {
+            event = new ReachedAchievementEvent();
+        } else if (name.equals(Events.BESTIARY)) {
+        	if (Debug.TEMP)
+        		;
+//            event = new BestiaryEvent();
+        }
 
-		if (event != null) {
-			event.init(entity, rpevent);
-		}
-		return event;
-	}
+        if (event != null) {
+            event.init(entity, rpevent);
+        }
+        return event;
+    }
 
-	/**
-	 * Creates events for normal Entities.
-	 *
-	 * @param entity  Entity
-	 * @param rpevent RPEvent
-	 * @return Event handler
-	 */
-	private static Event<Entity> createEventsForEntity(Entity entity, RPEvent rpevent) {
-		String name = rpevent.getName();
-		Event<Entity> event = null;
+    /**
+     * Creates events for normal Entities.
+     *
+     * @param entity  Entity
+     * @param rpevent RPEvent
+     * @return Event handler
+     */
+    private static Event<Entity> createEventsForEntity(Entity entity, RPEvent rpevent) {
+        String name = rpevent.getName();
+        Event<Entity> event = null;
 
-		if (name.equals("examine")) {
-			event = new ExamineEvent();
-		} else if (name.equals("show_item_list")) {
-			event = new ShowItemListEvent();
-		} else if (name.equals(Events.OUTFIT_LIST)) {
-			event = new ShowOutfitListEvent();
-		} else if (name.equals(Events.SOUND)) {
-			event = new SoundEvent();
-		} else if (name.equals("transition_graph")) {
-			event = new TransitionGraphEvent();
-		} else if (name.equals(Events.PLAYER_LOGGED_ON)) {
-			event = new PlayerLoggedOnEvent();
-		} else if (name.equals(Events.PLAYER_LOGGED_OUT)) {
-			event = new PlayerLoggedOutEvent();
-		} else if (name.equals(Events.VIEW_CHANGE)) {
-			event = new ViewChangeEvent();
-		}  else if (name.equals(Events.IMAGE)) {
-			event = new ImageEffectEvent();
-		} else if (name.equals(Events.PUBLIC_TEXT)) {
-			event = new EntityMessageEvent();
-		}  else if (name.equals(Events.GLOBAL_VISUAL)) {
-			event = new GlobalVisualEffectEvent();
-		}
+        if (name.equals("examine")) {
+            event = new ExamineEvent();
+        } else if (name.equals("show_item_list")) {
+            event = new ShowItemListEvent();
+        } else if (name.equals(Events.OUTFIT_LIST)) {
+        	if (Debug.TEMP)
+        		;
+//            event = new ShowOutfitListEvent();
+        } else if (name.equals(Events.SOUND)) {
+            event = new SoundEvent();
+        } else if (name.equals("transition_graph")) {
+            event = new TransitionGraphEvent();
+        } else if (name.equals(Events.PLAYER_LOGGED_ON)) {
+            event = new PlayerLoggedOnEvent();
+        } else if (name.equals(Events.PLAYER_LOGGED_OUT)) {
+            event = new PlayerLoggedOutEvent();
+        } else if (name.equals(Events.VIEW_CHANGE)) {
+            event = new ViewChangeEvent();
+        } else if (name.equals(Events.IMAGE)) {
+            event = new ImageEffectEvent();
+        } else if (name.equals(Events.PUBLIC_TEXT)) {
+            event = new EntityMessageEvent();
+        } else if (name.equals(Events.GLOBAL_VISUAL)) {
+            event = new GlobalVisualEffectEvent();
+        }
 
-		if (event != null) {
-			event.init(entity, rpevent);
-		}
-		return event;
-	}
+        if (event != null) {
+            event.init(entity, rpevent);
+        }
+        return event;
+    }
 }
