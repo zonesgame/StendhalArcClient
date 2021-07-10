503e30dc644c4d39b1a8a4bed6bb905533cb4849
zonesgame <32406374+zonesgame@users.noreply.github.com>
2021-07-10 17:31:03 +0800

    Update Member.java

--- core/src/games/stendhal/client/gui/group/Member.java
+++ core/src/games/stendhal/client/gui/group/Member.java
@@ -11,8 +11,8 @@
  ***************************************************************************/
 package games.stendhal.client.gui.group;
 
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
+import temp.javax.swing.event.ChangeEvent;
+import temp.javax.swing.event.ChangeListener;
 
 import games.stendhal.client.gui.LinearScalingModel;
 
@@ -21,142 +21,146 @@ import games.stendhal.client.gui.LinearScalingModel;
  * direct information about, ie. the players on the same zone.
  */
 class Member implements Comparable<Member> {
-	private final String name;
-	private boolean leader;
-	private boolean present;
-	/** Model for detecting which HP changes are significant */
-	private final LinearScalingModel hpModel = new LinearScalingModel();
-	/** Listener that needs to be notified about significant HP changes. */
-	private ChangeListener listener;
+    private final String name;
+    private boolean leader;
+    private boolean present;
+    /**
+     * Model for detecting which HP changes are significant
+     */
+    private final LinearScalingModel hpModel = new LinearScalingModel();
+    /**
+     * Listener that needs to be notified about significant HP changes.
+     */
+    private ChangeListener listener;
 
-	/**
-	 * Create a new member.
-	 *
-	 * @param name name of the buddy
-	 */
-	Member(String name) {
-		this.name = name;
-		hpModel.addChangeListener(new ChangeListener() {
-			@Override
-			public void stateChanged(ChangeEvent e) {
-				if (listener != null) {
-					// redispatch as a change coming from this member
-					listener.stateChanged(new ChangeEvent(Member.this));
-				}
-			}
-		});
-	}
+    /**
+     * Create a new member.
+     *
+     * @param name name of the buddy
+     */
+    Member(String name) {
+        this.name = name;
+        hpModel.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                if (listener != null) {
+                    // redispatch as a change coming from this member
+                    listener.stateChanged(new ChangeEvent(Member.this));
+                }
+            }
+        });
+    }
 
-	/**
-	 * Get the name of the member.
-	 *
-	 * @return name
-	 */
-	String getName() {
-		return name;
-	}
+    /**
+     * Get the name of the member.
+     *
+     * @return name
+     */
+    String getName() {
+        return name;
+    }
 
-	/**
-	 * Check if the member is the group leader.
-	 *
-	 * @return <code>true</code> if the member is the group leader,
-	 *	<code>false</code> otherwise
-	 */
-	boolean isLeader() {
-		return leader;
-	}
+    /**
+     * Check if the member is the group leader.
+     *
+     * @return <code>true</code> if the member is the group leader,
+     * <code>false</code> otherwise
+     */
+    boolean isLeader() {
+        return leader;
+    }
 
-	/**
-	 * Set the leader status of the group member.
-	 *
-	 * @param status the new leader status
-	 * @return <code>true</code> if the leader changed, <code>false</code>
-	 * 	otherwise
-	 */
-	boolean setLeader(boolean status) {
-		boolean changed = leader != status;
-		leader = status;
-		return changed;
-	}
+    /**
+     * Set the leader status of the group member.
+     *
+     * @param status the new leader status
+     * @return <code>true</code> if the leader changed, <code>false</code>
+     * otherwise
+     */
+    boolean setLeader(boolean status) {
+        boolean changed = leader != status;
+        leader = status;
+        return changed;
+    }
 
-	/**
-	 * Check if the member is present.
-	 *
-	 * @return <code>true</code> if the member is present, <code>false</code>
-	 * 	otherwise
-	 */
-	boolean isPresent() {
-		return present;
-	}
+    /**
+     * Check if the member is present.
+     *
+     * @return <code>true</code> if the member is present, <code>false</code>
+     * otherwise
+     */
+    boolean isPresent() {
+        return present;
+    }
 
-	/**
-	 * Set the member present or absent.
-	 *
-	 * @param present
-	 */
-	void setPresent(boolean present) {
-		this.present = present;
-	}
+    /**
+     * Set the member present or absent.
+     *
+     * @param present
+     */
+    void setPresent(boolean present) {
+        this.present = present;
+    }
 
-	/**
-	 * Get the ratio of the member's current HP vs her maximum HP. The value
-	 * is reliable only if the member is present.
-	 *
-	 * @return HP ratio
-	 */
-	float getHpRatio() {
-		return (float) hpModel.getValue();
-	}
+    /**
+     * Get the ratio of the member's current HP vs her maximum HP. The value
+     * is reliable only if the member is present.
+     *
+     * @return HP ratio
+     */
+    float getHpRatio() {
+        return (float) hpModel.getValue();
+    }
 
-	/**
-	 * Set the ratio of the member's current HP vs her maximum HP.
-	 *
-	 * @param ratio new HP ratio
-	 */
-	void setHpRatio(float ratio) {
-		hpModel.setValue(ratio);
-	}
+    /**
+     * Set the ratio of the member's current HP vs her maximum HP.
+     *
+     * @param ratio new HP ratio
+     */
+    void setHpRatio(float ratio) {
+        hpModel.setValue(ratio);
+    }
 
-	/**
-	 * Set the listener that should be notified about changes in the member.
-	 *
-	 * @param listener
-	 */
-	void setChangeListener(ChangeListener listener) {
-		this.listener = listener;
-	}
+    /**
+     * Set the listener that should be notified about changes in the member.
+     *
+     * @param listener
+     */
+    void setChangeListener(ChangeListener listener) {
+        this.listener = listener;
+    }
 
-	/**
-	 * Set the maximum HP representation. Used to detect which HP ratio changes
-	 * are significant.
-	 *
-	 * @param maxRepresentation
-	 */
-	void setMaxHPRepresentation(int maxRepresentation) {
-		hpModel.setMaxRepresentation(maxRepresentation);
-	}
+    /**
+     * Set the maximum HP representation. Used to detect which HP ratio changes
+     * are significant.
+     *
+     * @param maxRepresentation
+     */
+    void setMaxHPRepresentation(int maxRepresentation) {
+        hpModel.setMaxRepresentation(maxRepresentation);
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (obj instanceof Member) {
-			return name.equals(((Member) obj).name);
-		}
-		return false;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Member) {
+            return name.equals(((Member) obj).name);
+        }
+        return false;
+    }
 
-	@Override
-	public int hashCode() {
-		return name.hashCode();
-	}
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
 
-	@Override
-	public int compareTo(Member member) {
-		// There really should be only one leader, but check for consistent
-		// ordering
-		if (leader != member.leader) {
-			return (leader) ? -1 : 1;
-		}
+    @Override
+    public int compareTo(Member member) {
+        // There really should be only one leader, but check for consistent
+        // ordering
+        if (leader != member.leader) {
+            return (leader) ? -1 : 1;
+        }
 
-		return name.compareToIgnoreCase(member.name);
-	}
+        return name.compareToIgnoreCase(member.name);
+    }
 }
