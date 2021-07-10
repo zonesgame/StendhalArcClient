a2bcbf755e1bddfa08eac694e3cff082be15da21
zonesgame <32406374+zonesgame@users.noreply.github.com>
2021-07-10 17:30:50 +0800

    Update AbstractScalingModel.java

--- core/src/games/stendhal/client/gui/AbstractScalingModel.java
+++ core/src/games/stendhal/client/gui/AbstractScalingModel.java
@@ -14,51 +14,51 @@ package games.stendhal.client.gui;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
+import temp.javax.swing.event.ChangeEvent;
+import temp.javax.swing.event.ChangeListener;
 
 /**
  * A {@link ScalingModel} that implements ChangeListener handling, but nothing
  * else.
  */
 public abstract class AbstractScalingModel implements ScalingModel {
-	private final List<ChangeListener> listeners = new CopyOnWriteArrayList<ChangeListener>();
-	private int representation;
+    private final List<ChangeListener> listeners = new CopyOnWriteArrayList<ChangeListener>();
+    private int representation;
 
-	@Override
-	public void addChangeListener(ChangeListener listener) {
-		listeners.add(listener);
-	}
+    @Override
+    public void addChangeListener(ChangeListener listener) {
+        listeners.add(listener);
+    }
 
-	/**
-	 * Set the representation, and notify listeners if it changed.
-	 *
-	 * @param representation new representation
-	 * @return <code>true</code> if the representation changed, otherwise
-	 * 	<code>false</code>
-	 */
-	protected boolean setRepresentation(int representation) {
-		int oldRepresentation = this.representation;
-		this.representation = representation;
-		if (oldRepresentation != representation) {
-			fireChanged();
-			return true;
-		}
-		return false;
-	}
+    /**
+     * Set the representation, and notify listeners if it changed.
+     *
+     * @param representation new representation
+     * @return <code>true</code> if the representation changed, otherwise
+     * <code>false</code>
+     */
+    protected boolean setRepresentation(int representation) {
+        int oldRepresentation = this.representation;
+        this.representation = representation;
+        if (oldRepresentation != representation) {
+            fireChanged();
+            return true;
+        }
+        return false;
+    }
 
-	@Override
-	public int getRepresentation() {
-		return representation;
-	}
+    @Override
+    public int getRepresentation() {
+        return representation;
+    }
 
-	/**
-	 * Notify change listeners.
-	 */
-	protected void fireChanged() {
-		ChangeEvent e = new ChangeEvent(this);
-		for (ChangeListener listener : listeners) {
-			listener.stateChanged(e);
-		}
-	}
+    /**
+     * Notify change listeners.
+     */
+    protected void fireChanged() {
+        ChangeEvent e = new ChangeEvent(this);
+        for (ChangeListener listener : listeners) {
+            listener.stateChanged(e);
+        }
+    }
 }
