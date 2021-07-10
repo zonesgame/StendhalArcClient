e3cc9911f059894a9f9c04dd4edc1951dd6d811b
zonesgame <32406374+zonesgame@users.noreply.github.com>
2021-07-10 14:11:18 +0800

    Update Inspector.java

--- core/src/games/stendhal/client/entity/Inspector.java
+++ core/src/games/stendhal/client/entity/Inspector.java
@@ -12,9 +12,9 @@
  ***************************************************************************/
 package games.stendhal.client.entity;
 
-import games.stendhal.client.gui.SlotWindow;
+//import games.stendhal.client.gui.SlotWindow;
 import marauroa.common.game.RPSlot;
 
 public interface Inspector {
-	SlotWindow inspectMe(IEntity entity, RPSlot content, SlotWindow container, int width, int height);
+//	SlotWindow inspectMe(IEntity entity, RPSlot content, SlotWindow container, int width, int height);
 }
