89d2fbe45a8a8592cb9d2568af720f9ea1811b3f
zonesgame <32406374+zonesgame@users.noreply.github.com>
2021-07-10 16:35:24 +0800

    Update AchievementBoxFactory.java

--- core/src/games/stendhal/client/gui/j2d/AchievementBoxFactory.java
+++ core/src/games/stendhal/client/gui/j2d/AchievementBoxFactory.java
@@ -11,15 +11,14 @@
  ***************************************************************************/
 package games.stendhal.client.gui.j2d;
 
-import java.awt.AlphaComposite;
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Graphics2D;
-import java.awt.GraphicsConfiguration;
-import java.awt.GraphicsEnvironment;
-import java.awt.RenderingHints;
-import java.awt.geom.Rectangle2D;
-import java.awt.image.BufferedImage;
+import temp.Debug;
+import temp.java.awt.AlphaComposite;
+//import java.awt.Graphics2D;
+//import java.awt.GraphicsConfiguration;
+//import java.awt.GraphicsEnvironment;
+//import java.awt.RenderingHints;
+import temp.java.awt.geom.Rectangle2D;
+//import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.Locale;
 
@@ -50,7 +49,7 @@ public class AchievementBoxFactory {
 	private static final int IMAGE_PAD = 5;
 
 	/** Used for calculating the line metrics */
-	private static final Graphics2D graphics = (new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB)).createGraphics();
+//	private static final Graphics2D graphics = (new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB)).createGraphics();
 
 	/**
 	 * Create a sprite for a reached achievement
@@ -60,53 +59,57 @@ public class AchievementBoxFactory {
 	 * @return the drawn sprite
 	 */
 	public Sprite createAchievementBox(String title, String description, String category) {
-		final GraphicsConfiguration gc = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDefaultConfiguration();
-		// Get the category image
-		// initialize category image with empty image in case loading the image fails
-		BufferedImage categoryImage = gc.createCompatibleImage(32, 32, TransparencyMode.TRANSPARENCY);
-		String imageName = ACHIEVEMENT_IMAGE_FOLDER + category.toLowerCase(Locale.ENGLISH) + ".png";
-		try {
-			categoryImage = ImageIO.read(DataLoader.getResourceAsStream(imageName));
-		} catch (IOException e) {
-			Logger.getLogger(AchievementBoxFactory.class).error("Error loading achievement box image: " + imageName, e);
-		} catch (RuntimeException e) {
-			Logger.getLogger(AchievementBoxFactory.class).error("Error loading achievement box image: " + imageName, e);
-		}
-		// Calculate size for the message box
-		String fontName = WtWindowManager.getInstance().getProperty("ui.logfont", FONT_NAME);
-		Font font = new Font(fontName, Font.PLAIN, 14);
-		Font largeFont = font.deriveFont(20f);
-		Rectangle2D titleRect = largeFont.getStringBounds(title, graphics.getFontRenderContext());
-		Rectangle2D textRect = font.getStringBounds(description, graphics.getFontRenderContext());
-		int width = (int) Math.max(titleRect.getWidth(), textRect.getWidth())+categoryImage.getWidth();
-		int height = (int) Math.max(categoryImage.getHeight(), (titleRect.getHeight() + textRect.getHeight()));
-		width += 2 * SIDE_MARGIN + IMAGE_PAD;
-		height += TOP_MARGIN + BOTTOM_MARGIN;
+		if (Debug.TEMP)
+			;
+		return null;
 
-		// Create the background sprite
-		final BufferedImage image = gc.createCompatibleImage(width, height, TransparencyMode.TRANSPARENCY);
-		final Graphics2D g2d = image.createGraphics();
-		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		g2d.setComposite(AlphaComposite.Src);
-		BackgroundPainter bp = new BackgroundPainter(BACKGROUND);
-		bp.paint(g2d, width, height);
-
-		// Draw the texts
-		g2d.setColor(Color.BLACK);
-		g2d.setFont(largeFont);
-		g2d.drawString(title, SIDE_MARGIN + IMAGE_PAD + categoryImage.getWidth(), TOP_MARGIN + (int) titleRect.getHeight());
-
-		g2d.setFont(font);
-		g2d.drawString(description, SIDE_MARGIN + IMAGE_PAD + categoryImage.getWidth(), height - BOTTOM_MARGIN);
-
-		// Draw the image (the usable height starts right from the top)
-		int y = (height - BOTTOM_MARGIN - categoryImage.getHeight()) / 2 + TOP_MARGIN;
-		g2d.setComposite(AlphaComposite.SrcOver);
-		g2d.drawImage(categoryImage, SIDE_MARGIN, y, null);
-
-		g2d.dispose();
-
-		return new ImageSprite(image);
+//		final GraphicsConfiguration gc = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDefaultConfiguration();
+//		// Get the category image
+//		// initialize category image with empty image in case loading the image fails
+//		BufferedImage categoryImage = gc.createCompatibleImage(32, 32, TransparencyMode.TRANSPARENCY);
+//		String imageName = ACHIEVEMENT_IMAGE_FOLDER + category.toLowerCase(Locale.ENGLISH) + ".png";
+//		try {
+//			categoryImage = ImageIO.read(DataLoader.getResourceAsStream(imageName));
+//		} catch (IOException e) {
+//			Logger.getLogger(AchievementBoxFactory.class).error("Error loading achievement box image: " + imageName, e);
+//		} catch (RuntimeException e) {
+//			Logger.getLogger(AchievementBoxFactory.class).error("Error loading achievement box image: " + imageName, e);
+//		}
+//		// Calculate size for the message box
+//		String fontName = WtWindowManager.getInstance().getProperty("ui.logfont", FONT_NAME);
+//		Font font = new Font(fontName, Font.PLAIN, 14);
+//		Font largeFont = font.deriveFont(20f);
+//		Rectangle2D titleRect = largeFont.getStringBounds(title, graphics.getFontRenderContext());
+//		Rectangle2D textRect = font.getStringBounds(description, graphics.getFontRenderContext());
+//		int width = (int) Math.max(titleRect.getWidth(), textRect.getWidth())+categoryImage.getWidth();
+//		int height = (int) Math.max(categoryImage.getHeight(), (titleRect.getHeight() + textRect.getHeight()));
+//		width += 2 * SIDE_MARGIN + IMAGE_PAD;
+//		height += TOP_MARGIN + BOTTOM_MARGIN;
+//
+//		// Create the background sprite
+//		final BufferedImage image = gc.createCompatibleImage(width, height, TransparencyMode.TRANSPARENCY);
+//		final Graphics2D g2d = image.createGraphics();
+//		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+//		g2d.setComposite(AlphaComposite.Src);
+//		BackgroundPainter bp = new BackgroundPainter(BACKGROUND);
+//		bp.paint(g2d, width, height);
+//
+//		// Draw the texts
+//		g2d.setColor(Color.BLACK);
+//		g2d.setFont(largeFont);
+//		g2d.drawString(title, SIDE_MARGIN + IMAGE_PAD + categoryImage.getWidth(), TOP_MARGIN + (int) titleRect.getHeight());
+//
+//		g2d.setFont(font);
+//		g2d.drawString(description, SIDE_MARGIN + IMAGE_PAD + categoryImage.getWidth(), height - BOTTOM_MARGIN);
+//
+//		// Draw the image (the usable height starts right from the top)
+//		int y = (height - BOTTOM_MARGIN - categoryImage.getHeight()) / 2 + TOP_MARGIN;
+//		g2d.setComposite(AlphaComposite.SrcOver);
+//		g2d.drawImage(categoryImage, SIDE_MARGIN, y, null);
+//
+//		g2d.dispose();
+//
+//		return new ImageSprite(image);
 	}
 
 }
