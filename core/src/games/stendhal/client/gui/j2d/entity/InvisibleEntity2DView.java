9ba9f3c8cc1a8f9692a26af2250f977914405baf
zonesgame <32406374+zonesgame@users.noreply.github.com>
2021-07-10 16:35:15 +0800

    Update InvisibleEntity2DView.java

--- core/src/games/stendhal/client/gui/j2d/entity/InvisibleEntity2DView.java
+++ core/src/games/stendhal/client/gui/j2d/entity/InvisibleEntity2DView.java
@@ -12,11 +12,9 @@
  ***************************************************************************/
 package games.stendhal.client.gui.j2d.entity;
 
-
-import java.awt.Graphics2D;
-
 import games.stendhal.client.entity.IEntity;
 import games.stendhal.client.gui.styled.cursor.StendhalCursor;
+import temp.java.awt.Graphics;
 
 /**
  * The 2D view of an invisible entity.
@@ -25,51 +23,50 @@ import games.stendhal.client.gui.styled.cursor.StendhalCursor;
  */
 class InvisibleEntity2DView<T extends IEntity> extends Entity2DView<T> {
 
-	//
-	// Entity2DView
-	//
+    //
+    // Entity2DView
+    //
 
-	@Override
-	protected void buildRepresentation(T entity) {
-	}
+    @Override
+    protected void buildRepresentation(T entity) {
+    }
 
-	/**
-	 * Draw the entity (NOT!).
-	 *
-	 * @param g2d
-	 *            The graphics to drawn on.
-	 */
-	@Override
-	public void draw(final Graphics2D g2d) {
-		applyChanges();
-	}
+    /**
+     * Draw the entity (NOT!).
+     *
+     * @param g2d The graphics to drawn on.
+     */
+    @Override
+    public void draw(final Graphics g2d) {
+        applyChanges();
+    }
 
-	/**
-	 * Determines on top of which other entities this entity should be drawn.
-	 * Entities with a high Z index will be drawn on top of ones with a lower Z
-	 * index.
-	 *
-	 * Also, players can only interact with the topmost entity.
-	 *
-	 * @return The drawing index.
-	 */
-	@Override
-	public int getZIndex() {
-		return 5000;
-	}
+    /**
+     * Determines on top of which other entities this entity should be drawn.
+     * Entities with a high Z index will be drawn on top of ones with a lower Z
+     * index.
+     * <p>
+     * Also, players can only interact with the topmost entity.
+     *
+     * @return The drawing index.
+     */
+    @Override
+    public int getZIndex() {
+        return 5000;
+    }
 
-	@Override
-	public boolean isInteractive() {
-		return false;
-	}
+    @Override
+    public boolean isInteractive() {
+        return false;
+    }
 
-	/**
-	 * gets the mouse cursor image to use for this entity
-	 *
-	 * @return StendhalCursor
-	 */
-	@Override
-	public StendhalCursor getCursor() {
-		return null;
-	}
+    /**
+     * gets the mouse cursor image to use for this entity
+     *
+     * @return StendhalCursor
+     */
+    @Override
+    public StendhalCursor getCursor() {
+        return null;
+    }
 }
