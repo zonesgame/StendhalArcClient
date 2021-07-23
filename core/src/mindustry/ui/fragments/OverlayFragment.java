5ad088b60ca129108c672edef39caf5e38c01d5a
zonesgame <32406374+zonesgame@users.noreply.github.com>
2021-07-23 17:48:35 +0800

    Update OverlayFragment.java

--- core/src/mindustry/ui/fragments/OverlayFragment.java
+++ core/src/mindustry/ui/fragments/OverlayFragment.java
@@ -3,7 +3,9 @@ package mindustry.ui.fragments;
 import arc.*;
 import arc.scene.event.*;
 import arc.scene.ui.layout.*;
+import arc.z.util.ZonesAnnotate;
 import mindustry.*;
+import stendhal.test.ActionConfigFragment;
 
 /** Fragment for displaying overlays such as block inventories. */
 public class OverlayFragment{
@@ -16,17 +18,32 @@ public class OverlayFragment{
         group.touchable(Touchable.childrenOnly);
         inv = new BlockInventoryFragment();
         config = new BlockConfigFragment();
+        // stendhal
+        actions = new ActionConfigFragment();
     }
 
     public void add(){
         group.setFillParent(true);
-        Vars.ui.hudGroup.addChildBefore(Core.scene.find("overlaymarker"), group);
+//        Vars.ui.hudGroup.addChildBefore(Core.scene.find("overlaymarker"), group);
+        Core.scene.add(group);
 
         inv.build(group);
         config.build(group);
+        // stendhal
+        actions.build(group);
     }
 
     public void remove(){
         group.remove();
     }
+
+
+    // stendhal begon
+    public final ActionConfigFragment actions;
+
+    @ZonesAnnotate.ZAdd
+    public void lookAction(String[] actions) {
+
+    }
+    // stendhal end
 }
