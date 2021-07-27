0202afd03033311b0f511749d491dbb52af22ddb
zonesgame <32406374+zonesgame@users.noreply.github.com>
2021-07-27 22:22:49 +0800

    Update FlyOverAreaMapObject.java

--- core/src/games/stendhal/client/gui/map/FlyOverAreaMapObject.java
+++ core/src/games/stendhal/client/gui/map/FlyOverAreaMapObject.java
@@ -11,23 +11,22 @@
  ***************************************************************************/
 package games.stendhal.client.gui.map;
 
-import java.awt.Color;
-import java.awt.Graphics;
+import arc.graphics.Color;
+import temp.java.awt.Graphics;
 
 import games.stendhal.client.entity.IEntity;
+import temp.java.awt.geom.Rectangle2D;
 
 public class FlyOverAreaMapObject extends StaticMapObject {
-	/**
-	 * The colour of fly over areas (orange).
-	 */
-	private static final Color COLOR = new Color(212, 158, 72);
+	/**The colour of fly over areas (orange).*/
+	private static final Color COLOR = new Color(212 / 255f, 158 / 255f, 72 / 255f, 1f);
 
-	FlyOverAreaMapObject(IEntity entity) {
+	public FlyOverAreaMapObject(IEntity entity) {
 		super(entity);
 	}
 
 	@Override
-	void draw(Graphics g, int scale) {
-		draw(g, scale, COLOR, null);
+	void draw(Graphics g, Rectangle2D drawRect, float actorx, float actory, float scale) {
+		draw(g, drawRect, actorx, actory, scale, COLOR, null);
 	}
 }
