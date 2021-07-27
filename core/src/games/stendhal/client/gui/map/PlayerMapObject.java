a31871fc94afd6fd88d115c77522e2812988dc9d
zonesgame <32406374+zonesgame@users.noreply.github.com>
2021-07-27 22:23:33 +0800

    Update PlayerMapObject.java

--- core/src/games/stendhal/client/gui/map/PlayerMapObject.java
+++ core/src/games/stendhal/client/gui/map/PlayerMapObject.java
@@ -12,38 +12,32 @@
  ***************************************************************************/
 package games.stendhal.client.gui.map;
 
-import java.awt.Color;
-import java.awt.Graphics;
+import arc.graphics.Color;
+import temp.Debug;
+import temp.java.awt.Graphics;
 
 import games.stendhal.client.entity.EntityChangeListener;
 import games.stendhal.client.entity.IEntity;
 import games.stendhal.client.entity.Player;
 import games.stendhal.client.entity.RPEntity;
 import games.stendhal.client.entity.User;
+import temp.java.awt.geom.Rectangle2D;
 
-class PlayerMapObject extends RPEntityMapObject {
-	/**
-	 * The color of the player (blue).
-	 */
-	private static final Color COLOR_USER = Color.BLUE;
-	/**
-	 * The color of other players (white).
-	 */
-	private static final Color COLOR_PLAYER = Color.WHITE;
-	/**
-	 * The color of group players, if visible (grayish).
-	 */
-	private static final Color COLOR_GROUP = new Color(99, 61, 139);
-	/**
-	 * The color of ghostmode players, if visible (gray).
-	 */
-	private static final Color COLOR_GHOST = Color.GRAY;
+public class PlayerMapObject extends RPEntityMapObject {
+	/**The color of the player (blue).*/
+	private static final Color COLOR_USER = Color.blue;
+	/**The color of other players (white).*/
+	private static final Color COLOR_PLAYER = Color.white;
+	/**The color of group players, if visible (grayish).*/
+	private static final Color COLOR_GROUP = new Color(99 / 255f, 61 / 255f, 139 / 255f, 1);
+	/**The color of ghostmode players, if visible (gray).*/
+	private static final Color COLOR_GHOST = Color.gray;
 
-	PlayerMapObject(final IEntity entity) {
+	public PlayerMapObject(final IEntity entity) {
 		super(entity);
 
 		if (entity instanceof User) {
-			drawColor = COLOR_USER;
+			curColor = COLOR_USER;
 		} else if (entity instanceof Player) {
 			final Player player = (Player) entity;
 
@@ -62,49 +56,48 @@ class PlayerMapObject extends RPEntityMapObject {
 	}
 
 	@Override
-	void draw(final Graphics g, final int scale) {
-		if ((drawColor != COLOR_GHOST) || User.isAdmin()) {
-			super.draw(g, scale);
+	void draw(final Graphics g, final Rectangle2D drawRect, final float actorx, final float actory, final float scale) {
+		if ((curColor != COLOR_GHOST) || User.isAdmin()) {
+			super.draw(g, drawRect, actorx, actory, scale);
 		}
 	}
 
-	/**
-	 * Select a color for drawing the player depending on
-	 * ghostmode status.
-	 *
+	/**Select a color for drawing the player depending on ghostmode status.
 	 * @param player
 	 */
 	private void choosePlayerColor(final Player player) {
 		if (player.isGhostMode()) {
-			drawColor = COLOR_GHOST;
+			curColor = COLOR_GHOST;
 		} else {
 			if (User.isPlayerInGroup(player.getName())) {
-				drawColor = COLOR_GROUP;
+				curColor = COLOR_GROUP;
 			} else {
-				drawColor = COLOR_PLAYER;
+				curColor = COLOR_PLAYER;
 			}
 		}
 	}
 
-	/**
-	 * Draws a player using given color.
-	 *
+	/**Draws a player using given color.
 	 * @param g The graphics context
 	 * @param scale Scaling factor
 	 * @param color The draw color
 	 */
 	@Override
-	void draw(final Graphics g, final int scale,  final Color color) {
-		int mapX = worldToCanvas(x, scale);
-		int mapY = worldToCanvas(y, scale);
-		final int scale_2 = scale / 2;
-		final int size = scale_2 + 2;
+	void draw(final Graphics g, final Rectangle2D drawRect, final float actorx, final float actory, final float scale,  final Color color) {
+		if (Debug.NOTE1) {
+			super.draw(g, drawRect, actorx, actory, scale, color);
+			return;
+		}
+		float mapX = worldToCanvas(x, scale);
+		float mapY = worldToCanvas(y, scale);
+		final float scale_2 = scale / 2;
+		final float size = scale_2 + 2;
 
 		mapX += scale_2;
 		mapY += scale_2;
 
-		g.setColor(color);
-		g.drawLine(mapX - size, mapY, mapX + size, mapY);
-		g.drawLine(mapX, mapY - size, mapX, mapY + size);
+//		g.setColor(color);
+//		g.drawLine(mapX - size, mapY, mapX + size, mapY);
+//		g.drawLine(mapX, mapY - size, mapX, mapY + size);
 	}
 }
