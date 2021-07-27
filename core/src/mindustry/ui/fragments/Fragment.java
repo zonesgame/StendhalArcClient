30328ff0164c64fe08a1fb1e03f464519ed9c763
zonesgame <32406374+zonesgame@users.noreply.github.com>
2021-07-27 22:23:25 +0800

    Update Fragment.java

--- core/src/mindustry/ui/fragments/Fragment.java
+++ core/src/mindustry/ui/fragments/Fragment.java
@@ -1,6 +1,11 @@
 package mindustry.ui.fragments;
 
+import arc.Core;
+import arc.Events;
+import arc.scene.Element;
 import arc.scene.Group;
+import arc.scene.event.VisibilityListener;
+import mindustry.game.EventType;
 
 public abstract class Fragment{
     public abstract void build(Group parent);
