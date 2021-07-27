45194a737184441adb200b578a959a7ae789b635
zonesgame <32406374+zonesgame@users.noreply.github.com>
2021-07-27 22:23:11 +0800

    Create GroupFragment.java

---
+++ core/src/stendhal/test/GroupFragment.java
@@ -0,0 +1,15 @@
+package stendhal.test;
+
+import arc.Events;
+import arc.scene.Element;
+import arc.scene.event.VisibilityListener;
+import arc.scene.ui.layout.Table;
+import mindustry.game.EventType;
+import mindustry.ui.fragments.Fragment;
+
+public abstract class GroupFragment extends Fragment {
+
+    protected Table root;
+
+
+}
